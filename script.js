0629c0a96552602a20e24f4d868f5d50e4a69ade
lucashandekyn <lucas.handekyn@gmail.com>
2020-10-07 22:17:28 +0200

    modified script.js

--- script.js
+++ script.js
@@ -34,11 +34,14 @@ function logKey(e) {
 
 
 function updateSnake() { 
+    const snake = [[100,100],null]; 
+    const crd2 = snake[0];
     x += dx * speed;
     y += dy * speed;   
-    const snake = [[x,y],[a,b]];  
-    //snake.unshift([x,y]);
-    //snake.pop();    
+    snake.pop();
+    snake.unshift(crd2);
+    snake.unshift([x,y]);
+    snake.pop();
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, 640, 640);
     ctx.beginPath();
@@ -47,35 +50,8 @@ function updateSnake() {
         ctx.arc( snake[i][0], snake[i][1], 10, 0, 2 * Math.PI);
     }
     ctx.stroke();
-    if (dx==1){
-        a = x-20;
-    }else if(dx==-1){
-        a = x+20;
-    }else if(dy==1){
-        b = y-20;
-    }else if(dy==-1){
-        b = y+20;
-    }
         
 }
-
-/*function draw() { 
-    console.log(snake[0][0]);
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, 640, 640);
-    drawSnake(ctx);
-}*/
-
-/*function drawSnake(ctx) {
-    ctx.beginPath();
-    ctx.strokeStyle = "#FF0000";
-    ctx.arc(snake[0][0],snake[0][1], 10, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.strokeStyle = "#FF0000";
-    ctx.arc(x-20, y, 10, 0, 2 * Math.PI);
-    ctx.stroke();
-}*/
 
 function update() {
     updateSnake();
@@ -86,3 +62,22 @@ function run() {
 }
 
 setInterval(run, 1000 / 30)
+
+
+/*function draw() { 
+    console.log(snake[0][0]);
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, 640, 640);
+    drawSnake(ctx);
+}
+
+function drawSnake(ctx) {
+    ctx.beginPath();
+    ctx.strokeStyle = "#FF0000";
+    ctx.arc(snake[0][0],snake[0][1], 10, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "#FF0000";
+    ctx.arc(x-20, y, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+}*/
