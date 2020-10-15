325b8f338e077c42b0ff21be60f5ca25933fafc7
lucashandekyn <lucas.handekyn@gmail.com>
2020-10-15 10:10:46 +0200

    modified script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener('keydown', logKey);
 
 var speed = 5;
-var crdxf = 0;
-var crdyf = 0;
+var crdxf = 265;
+var crdyf = 387;
 const food = [null];
 var dx = 0;
 var dy = 0;
-var bdy = 7;
+var bdy = 2;
+Boolean eaten = false;
 const snake = [[100,100],[100,100]]; 
 
 function logKey(e) {
-  //console.log(e.code);
   switch (e.key) {
     case "ArrowLeft":
         dx = -1;
@@ -31,9 +31,20 @@ function logKey(e) {
     }  
 }
 
+function score() {
+    switch(eaten) {
+        case true:
+            body += 1;
+            crdFood();
+            break
+        case false:
+
+    }
+}
+
 function crdFood() {
-    crdxf = Math.floor(Math.random() * 640) + 1;
-    crdyf = Math.floor(Math.random() * 640) + 1;
+    crdxf = Math.floor(Math.random() * 640) + 20;
+    crdyf = Math.floor(Math.random() * 640) + 20;
     food.unshift([crdxf,crdyf]);
     food.pop();
 }
@@ -43,7 +54,7 @@ function drawFood() {
     ctx.clearRect(0, 0, 640, 640); 
     ctx.fillStyle = "00FF00";
     ctx.beginPath();
-    ctx.arc( crdxf, crdyf, 10, 0, 2 * Math.PI);
+    ctx.arc( food[0][0], food[0][1], 10, 0, 2 * Math.PI);
     ctx.fill();
 }
 
@@ -76,11 +87,13 @@ function updateSnake() {
 }
 
 function updateFood() {
+    score();
     drawFood();
 }
 
 function update() {
     updateSnake();
+    updateFood();
 }
 
 function run() {
