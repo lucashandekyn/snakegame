3263da23b61f5c5e7b07e7f8bbc3d43062ae6f8c
lucashandekyn <lucas@handekyn.com>
2020-11-01 23:30:17 +0100

    test

--- script.js
+++ script.js
@@ -10,11 +10,7 @@ var dy = 0;
 var bdy = 3;
 var eaten = false;
 const snake = [[100,100],[100,100]]; 
-const crdx = [0];
-const crdy = [0];
-var mtrxlngth = 32;
-const matrixx = [[1]];
-const matrixy = [[1]];
+const matrix = [[[20],[20],[false]],[[20],[20],[false]]];
 
 function logKey(e) {
   switch (e.key) {
@@ -55,69 +51,19 @@ function score() {
     }
 }
 
-/*function crdFood() {
-    crdxf = Math.floor(Math.random() * 640) + 20;
-    crdyf = Math.floor(Math.random() * 640) + 20;
-    food.unshift([crdxf,crdyf]);
-    food.pop();
-}
-
-function drawFood() {
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#00FF00";
-    ctx.beginPath();
-    ctx.arc( food[0][0], food[0][1], 10, 0, 2 * Math.PI);
-    ctx.fill();
-}
-
-/*function lenghtsnake() {
-    var l = snake.length;
-    if (l==bdy){
-        snake.pop();
-    }
-}*/
-
-function crdSnake() {
-    crdx.unshift( [crdx[0]+ dx] );
-    crdy.unshift( [crdy[0] + dy] );
-    if (crdx.lenght >= bdy) {
-        crdx.pop();
-        crdy.pop();
-    }
-}
-
-function drawSnake() {
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, 640, 640);  
-    //for (i=0; i <= snake.length-1; i++){
-        ctx.fillStyle = "#FF0000";
-        ctx.beginPath();
-        ctx.arc( crdx[0]*20, crdy[0]*20, 10, 0, 2 * Math.PI);
-        ctx.fill();
-    //}
-}
 
 function createMatrix() {
-    for (i=2; i<=mtrxlngth; i++) {
-        if (matrixx.length < 32) {
-            matrixx.push([i]);
-        }
-    }
-    for (i=2; i<=mtrxlngth; i++) {
-        if (matrixy.length < 32) {
-            matrixy.push([i]);
+    for (i=2; i<=matrix.length; i++) {
+        if (matrix.length < 32) {
+            matrix.push([i]);
         }
     }
 }
 
-/*function drawGame() {
-    drawFood();
-    drawSnake();
-}*/
 
 function updateSnake() { 
-    crdSnake(); 
-    drawSnake();
+    //crdSnake(); 
+    //drawSnake();
 }
 
 /*function updateFood() {
