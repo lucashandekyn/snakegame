6c071b7c4bc9aaf92356877aa0528544bc7dc4cc
Lucas Handekyn <lucas.handekyn@gmail.com>
2022-05-21 19:11:13 +0200

    made array system for the snake

--- script.js
+++ script.js
@@ -1,74 +1,80 @@
 document.addEventListener('keydown', logKey);
 
-var count = 0;
-var speed = 2;
+var count = 27;
 
-var world = Array.from(Array(32), () => Array.from(Array(32), () => 0));
+
+var speed = 5;
+
+
+var food = [9, 9];
 
 var dx = 0;
 var dy = 0;
 
 var px = 1;
 var py = 1;
+var snake = [[1, 1], [px, py]];
 
 function logKey(e) {
-  switch (e.key) {
-    case "ArrowLeft":
-        dx = -1;
-        dy = 0; 
-        break;
-    case "ArrowRight":
-        dx = +1;
-        dy = 0;
-        break;
-    case "ArrowUp":
-        dy = -1;
-        dx = 0;
-        break;
-    case "ArrowDown":
-        dy = +1;
-        dx = 0;
-        break;
-    }  
+    switch (e.key) {
+        case "ArrowLeft":
+            dx = -1;
+            dy = 0;
+            break;
+        case "ArrowRight":
+            dx = +1;
+            dy = 0;
+            break;
+        case "ArrowUp":
+            dx = 0;
+            dy = -1;
+            break;
+        case "ArrowDown":
+            dx = 0;
+            dy = +1;
+            break;
+    }
     //console.log(e.key);
 }
 
 
 COLORS = { 0: "black", 1: "red", 2: "green" };
 
-function drawWorld() {
+function draw(array1, array2) {
     const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, 640, 640);  
-    for (i=0; i < 32; i++){
-        for (j=0; j < 32; j++){            
-            ctx.fillStyle = COLORS[world[i][j]];
-            ctx.fillRect(i*20, j*20, 20, 20);
-        }
+    ctx.clearRect(0, 0, 640, 640);
+    ctx.rect(0, 0, 640, 640)
+    ctx.fillStyle = COLORS[0];
+    ctx.fill();
+    for (i = 0; i < array1.length; i++) {
+        ctx.fillStyle = COLORS[1];
+        ctx.fillRect(array1[i][0] * 20, array1[i][1] * 20, 20, 20);
     }
+    ctx.fillStyle = COLORS[2];
+    ctx.fillRect(array2[0] * 20, array2[1] * 20, 20, 20);
 }
 
-function updateSnake() { 
-    world[px][py] = 0;
+
+function updateSnake() {
     px += dx;
     py += dy;
-    world[px][py] = 1;
+    if (snake.length >= count) {
+        snake.shift()
+    }
+    snake.push([px, py]);
 }
 
-function updateFood() {
-    world[9][9] = 2;
+function gameOver() {
+
 }
 
 function update() {
-    updateFood();
     updateSnake();
-
-    //createMatrix();
-    drawWorld();
+    draw(snake, food);
 }
 
 function run() {
     update();
-    count+=1;
 }
 
 setInterval(run, 1000 / speed)
